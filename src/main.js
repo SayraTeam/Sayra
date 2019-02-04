58048f4db782bc2076dca18e83f1314e79ffccda
tibdev78 <tibdev78@gmail.com>
2019-02-04 21:51:03 +0100

    delete error build

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import App from './App';
-import Axios from 'axios';
-import VueAxios from 'vue-axios';
 import router from './router'
 import store from './store/index';
 import registerServiceWorker from './registerServiceWorker'
 
-Vue.use(VueAxios, Axios, registerServiceWorker)
+Vue.use(registerServiceWorker)
 
 new Vue({
     render: h => h(App),
