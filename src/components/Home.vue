c2ae26fb679bc1fdaa61233f5e77f480910157e1
Méryl <meryl.valier@gmail.com>
2019-02-06 17:49:55 +0100

    typo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault" :class="{show: show == true}">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="articles">Giphe<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="articles">Gifs<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
