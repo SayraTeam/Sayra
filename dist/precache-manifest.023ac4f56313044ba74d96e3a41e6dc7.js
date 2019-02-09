a180cb08be321cbde25b94a6f829a892c68c668d
tibdev78 <40746565+tibdev78@users.noreply.github.com>
2019-02-09 13:34:02 +0100

    try test dist (#22)

---
+++ dist/precache-manifest.023ac4f56313044ba74d96e3a41e6dc7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bfa69d57d0e2389f098",
+    "url": "/js/app.3d172699.js"
+  },
+  {
+    "revision": "6d2fa7dd9cb3e148b41d",
+    "url": "/js/chunk-vendors.35894acf.js"
+  },
+  {
+    "revision": "eb297e18fea025fad4443902a343894a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c31c05b5704b61a9211e6594e2de04d2",
+    "url": "/css/sayra.css"
+  },
+  {
+    "revision": "1bdef0b5837fe56734fd7d7d93a83ee5",
+    "url": "/img/default.png"
+  },
+  {
+    "revision": "e2702f419c4fb2cd6aa55cf67ff6a31b",
+    "url": "/img/default2.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9557cced21e7985342ecfbe3f1daec39",
+    "url": "/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "7e923ad223e9f33e54d22e50cf2bcce5",
+    "url": "/css/bootstrap.min.css"
+  },
+  {
+    "revision": "edc48c74e66bd8b6153225f785e8116b",
+    "url": "/img/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "0a02bd58974b7dabde5d3e0d6b7f5fa5",
+    "url": "/img/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "3030828d512e5e0ef9fc48935bdffd48",
+    "url": "/img/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "1af9432e6f0de9b65e085f442222a1ec",
+    "url": "/img/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "f63be42941df1ea20c750b27a9a2bfed",
+    "url": "/img/splashscreens/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "3cbaf2a7640718a9244f6931bfd8a1f5",
+    "url": "/img/img1.jpg"
+  }
+];
