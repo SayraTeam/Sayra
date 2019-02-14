f4e52bbfbab53a0eaed7ea2053ea8e9672490310
tibdev78 <tibdev78@gmail.com>
2019-02-15 00:09:20 +0100

    change configuration service-worker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store/index';
-import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+    pwa: {
+        // configure the workbox plugin
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'public/service-worker.js',
+        }
+    }
+};
