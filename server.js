2868175b4d5f36488f2dda6809a838c0199c95dc
tibdev78 <tibdev78@gmail.com>
2019-02-15 14:53:56 +0100

    registerSW2

--- server.js
+++ server.js
@@ -4,22 +4,23 @@ const path = require('path');
 const history = require('connect-history-api-fallback');
 
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
+app.use(
+    history({
+        disableDotRule: true,
+        verbose: true,
+    })
+);
 app.use(staticFileMiddleware);
 
-app.get('/', function (req, res) {
-    res.render(path.join(__dirname + '/dist/index.html'))
-})
+app.get('/', function(req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 
-var server = app.listen(process.env.PORT || 5000, function () {
-    var port = server.address().port
-    console.log('App now running on port', port)
-})
-
+var server = app.listen(process.env.PORT || 5000, function() {
+    var port = server.address().port;
+    console.log('App now running on port', port);
+});
